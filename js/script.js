3f91e5769879eef564ad43927c849c8a3bb4f79c
Namita Dongre <ndongre@andrew.cmu.edu>
2020-01-29 08:52:20 -0500

    Project Rwanda Website Files - 2018

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+$(document).ready(function(){
+     $(window).scroll(function () {
+            if ($(this).scrollTop() > 30) {
+                $('#back-to-top').fadeIn();
+            } else {
+                $('#back-to-top').fadeOut();
+            }
+    });
+        // scroll body to 0px on click
+    $('#back-to-top').click(function () {
+            $('#back-to-top').tooltip('hide');
+            $('body,html').animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+    });
+
+    $(document).ready(function(){
+        $('[data-toggle="tooltip"]').tooltip(); 
+    });
+
+});
